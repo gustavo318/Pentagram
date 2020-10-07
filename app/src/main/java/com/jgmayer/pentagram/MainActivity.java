5fc95b33a00a993c30ab9f57fae4b0d078c04dd1
gustavo318 <jgusmayer@gamil.com>
2020-10-07 02:15:43 -0300

    Initial commit

---
+++ app/src/main/java/com/jgmayer/pentagram/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jgmayer.pentagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
